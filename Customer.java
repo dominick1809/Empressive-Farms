52d5dc4b8a73b3b7cd1c814003f38b797f4858e1
Anisah02 <118861260+Anisah02@users.noreply.github.com>
2022-11-25 15:25:56 -0500

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -5,12 +5,13 @@ class Customer{
     private int ID;
     private String password;
     private ArrayList<Order> orders;
+    private String cinfo;
 
     // Class constructor
     public Customer(){}
 
     public boolean checkPassword(String pswrd){
-        return true;
+        return pswrd == password ;
     }
 
     public Customer findCustomer(String name){
@@ -21,9 +22,13 @@ class Customer{
         return orders;
     }
 
-    public void placeOrder(){}
+    public void placeOrder(Order O1){
+        orders.add (O1);
+       
+    }
 
     public String toString(){
-        return "hello";
+        String cinfo= (String.ValueOf(ID)+" " +name + " " +password " "+orders)
+        return "cinfo"
     }
 }
