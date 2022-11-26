378a6d62cd046e7bd797917867b7fb2e73cb2856
Anisah02 <118861260+Anisah02@users.noreply.github.com>
2022-11-25 23:29:36 -0500

    Update Product.java

--- Product.java
+++ Product.java
@@ -4,22 +4,32 @@ class Product {
     private float quantity;
     private String category;
     private String type;
+    private int id;
 
     // Class constructor
     public Product(){}
 
     public boolean isAvailable(quantity_r){
-        if (quantity ==0 and quantity_r >= quantity){
+        if (quantity ==0 and quantity_r <= quantity){
             return true;
         }
         else{
             system.out.println("Quantity requested cannot be fulfilled")
         }
     }
+    public int getId()
+    {
+        return id;
+    }
 
-    public void updateProduct(){}
+    public void updateProduct(){
+    this.productorder = Order.updateorder
+        if ( quantity_r <= quantity ){
+        }
+    }
 
     public String toString(){
-        return "hello";
+        pinfo= (
+        return name + unit price;
     }
 }
