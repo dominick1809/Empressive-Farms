5bd29991f4b143ffe17ae42e3deb98eeb266d5df
clomitch <119002333+clomitch@users.noreply.github.com>
2022-11-28 17:16:18 -0500

    Add files via upload

--- Customer.java
+++ Customer.java
@@ -1,21 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Customer{
     private String name;
     private int ID;
     private String password;
-    private ArrayList<Order> orders;
-    private String cinfo;
+    private ArrayList<Order> orders;    // change to list of id for the order so avoid duplication of files
+    private static ArrayList<Customer> clist;
 
-    // Class constructor
-    public Customer(){}
-
-    public boolean checkPassword(String pswrd){
-        return pswrd == password ;
+    // Class constructors
+    public Customer(String name, String password){
+        this.name = name;
+        this.password = password;
+        if(clist.size() == 0){
+            fetch_data();
+        }
+        ID = clist.size() + 1;
+        clist.add(this);
     }
 
-    public Customer findCustomer(String name){
-        return this;
+    // constructor to be used by file method
+    private Customer(int ID, String name, String password){
+        orders = new ArrayList<Order>();
+        this.ID = ID;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean checkPassword(String pswrd){
+        return pswrd == password;
+    }
+
+    public static Customer findCustomer(String name){
+        for(Customer c: clist){
+            if(c.getName() == name){
+                return c;
+            }
+        }
+        return null;
     }
 
     public ArrayList<Order> getOrders(){
@@ -23,12 +52,47 @@ class Customer{
     }
 
     public void placeOrder(Order O1){
-        orders.add (O1);
-       
+        orders.add(O1);
+        for(Product p : O1.getProducts()){
+            Product pr = Product.findProduct(p.getID());
+            p.updateProduct(new Product(p.getName(), p.getPrice(), pr.getQty() - p.getQty(), p.getCategory(), p.getType()));
+        }
     }
 
     public String toString(){
-        String cinfo= (String.ValueOf(ID)+" " +name + " " +password " "+orders)
-        return "cinfo"
+        String str = String.valueOf(ID) + " " + name + " " + password;
+        for( Order o : orders){
+            str += String.valueOf(o.getID());
+        }
+        return str;
+    }
+
+    private void fetch_data(){
+        try{
+            Scanner scan = new Scanner(new File("Customer.dat"));
+            while(scan.hasNext()){
+                String[] c = scan.nextLine().split(" ");
+                Customer c1 = new Customer(Integer.valueOf(c[0]),c[1],c[2]);
+                if(c.length > 3){
+                    for(int i = 0; i<c.length; i++){
+                        Order o = Order.findOrder(Integer.valueOf(c[i+3]));
+                        if(o != null){
+                            c1.placeOrder(o);
+                        }
+                    }
+                }
+                clist.add(c1);
+            }
+        }catch(FileNotFoundException fe){}
+    }
+
+    public void saveData(){
+        try{
+            PrintWriter pen = new PrintWriter("Customer.dat");
+            for(Customer c : clist){
+                pen.println(c);
+            }
+            pen.close();
+        }catch(FileNotFoundException fe){}
     }
 }
--- Order.java
+++ Order.java
@@ -1,23 +1,101 @@
-import java.sql.Date;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
 
 class Order {
     private int orderID;
     private ArrayList<Product> products;
     private String paymentType;
-    private Date pd_Date;
-    private boolean status;
+    //private Date pd_Date;
+    private boolean status = false;
+    private int cID;                    // to identify the customer
+    private static ArrayList<Order> olist;
 
-    // Class constructor
-    public Order(){}
-
-    public boolean isFulfilled(){
-        return true;
+    // Class constructors
+    public Order(String ptype, int cID, ArrayList<Product> products){
+        this.products = products;
+        paymentType = ptype;
+        this.cID = cID;
+        //pd_Date = Date.l; //
+        if(olist.size() == 0){
+            fetch_data();
+        }
+        orderID = olist.size();
+        olist.add(this);
     }
 
-    public void updateOrder(){}
+    // Constructor to be used by fetch data
+    private Order(int ID, String ptype, int cID, boolean status){
+        orderID = ID;
+        paymentType = ptype;
+        this.cID = cID;
+        //this.pd_Date = pd_Date;
+        this.status = status;
+    }
 
+    public int getID(){
+        return orderID;
+    }
+
+    public ArrayList<Product> getProducts(){
+        return products;
+    }
+
+    public boolean isFulfilled(){
+        return status;
+    }
+
+    public void fulfillOrder(){
+        status = true;
+    }
+
+    public void updateOrder(Order o){
+        for(Order o1: olist){
+            if(o1.getID() == o.getID()){
+                o1 = o;
+            }
+        }
+    }
+
+    public static Order findOrder(int ID){
+        for(Order o : olist){
+            if(o.getID() == ID){
+                return o;
+            }
+        }
+        return null;
+    }
+    
     public String toString(){
-        return "hello";
+        String str = String.valueOf(orderID) + " " + paymentType + " " + String.valueOf(cID) + " " + String.valueOf(status);
+        return str;
+    }
+
+    private void fetch_data(){
+        try{
+            Scanner scan = new Scanner(new File("Order.dat"));
+            while(scan.hasNext()){
+                String[] o = scan.nextLine().split(" ");
+                Order o1 = new Order(Integer.valueOf(o[0]),o[1],Integer.valueOf(o[2]), Boolean.valueOf(o[3]));
+                for(int i = 0; i+4<o.length; i+=5){
+                    Product p1 = new Product(o[1], Float.valueOf(o[2]), Float.valueOf(o[3]),o[4],o[5]);
+                    products.add(p1);
+                }
+                olist.add(o1);
+            }
+        }catch(FileNotFoundException fe){}
+    }
+
+    public void saveData(){
+        try{
+            PrintWriter pen = new PrintWriter("Order.dat");
+            for(Order o : olist){
+                pen.println(o);
+            }
+            pen.close();
+        }catch(FileNotFoundException fe){}
     }
 }
